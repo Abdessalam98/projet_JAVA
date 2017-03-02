ab884c9441262617441878107e4facc77bf13b08
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2017-03-02 16:17:05 +0100

    add_condition_100

--- Numbers.java
+++ Numbers.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Numbers{
 	 
 	int nb_magic;
-	int coups_total=5;
+	int coups_total=8;
 	int coup_init=1;
 	int nb_chosen;
 	int cp_essai=coups_total;
@@ -18,48 +18,48 @@ public class Numbers{
 	}
 	
 	public void start(){
-	while(verif){
-		try {
+		while(verif){
+			try {
 	
-		Scanner input=new Scanner (System.in);
+				Scanner input=new Scanner (System.in);
 		
-		for(this.coup_init=1;this.coups_total>=this.coup_init;this.coup_init++){
-			this.cp_essai--;
-		System.out.println("Essai n° "+this.coup_init+"\nIl vous reste "+(this.cp_essai)+" essai(s)");
-		System.out.println("Entrez le nombre:");
+				for(this.coup_init=1;this.coups_total>=this.coup_init;this.coup_init++){
+					this.cp_essai--;
+					System.out.println("Essai n° "+this.coup_init+"\nIl vous reste "+(this.cp_essai)+" essai(s)");
+					System.out.println("Entrez le nombre:");
 		
-		this.nb_chosen=input.nextInt();
-		if (this.cp_essai==0){
-			System.out.println("Vous avez perdu \nLe nombre magique était "+this.nb_magic);
-		}else{
-			if (this.nb_chosen>this.nb_magic){
-				System.out.println("Trop grand");
+					this.nb_chosen=input.nextInt();
+					if (this.cp_essai==0){
+						System.out.println("Vous avez perdu \nLe nombre magique était "+this.nb_magic);
+					}else{
+						if (this.nb_chosen>this.nb_magic){
+							System.out.println("Trop grand");
+						}
+						if (this.nb_chosen>100){
+							System.out.println("Rappel : le nombre est compris entre 1 et 100");
+						}
+						if (this.nb_chosen<this.nb_magic){
+							System.out.println("Trop petit");
+						}
+						if (this.nb_chosen==this.nb_magic){
+							System.out.println("Bravo !!! Vous avez gagné");
+							break;
+						}
+					}
+		
+				}
+				input.close();
+			} catch (Exception e) {
+				System.out.println("\nSaisie incorrecte. Le jeu recommence...\n");
+				this.coup_init=1;
+				this.cp_essai=5;
+				continue;
 			}
-			if (this.nb_chosen<this.nb_magic){
-				System.out.println("Trop petit");
-			}
-			if (this.nb_chosen==this.nb_magic){
-				System.out.println("Bravo !!! Vous avez gagné");
-				break;
-			}
+			break;
 		}
-		
-		}
-		input.close();
-	} catch (Exception e) {
-		System.out.println("\nSaisie incorrecte. Le jeu recommence...\n");
-		this.coup_init=1;
-		this.cp_essai=5;
-		continue;
-	}
-		break;
-	}
 	}
 
 		
-	
-	
-	
 	public static void main(String[] args) {
 		Numbers jeu=new Numbers();
 		jeu.rand_val();
