91f9c25071b7ffeb18ff2fc1317f65eab0689788
abdeldouhi <abdessamadov@hotmail.fr>
2017-03-02 15:42:35 +0100

    update

--- Numbers.java
+++ Numbers.java
@@ -1,10 +1,4 @@
 import java.util.*;
-import java.awt.Graphics;
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import javax.swing.JTextField;
 
 
 public class Numbers{
@@ -14,6 +8,7 @@ public class Numbers{
 	int coup_init=1;
 	int nb_chosen;
 	int cp_essai=coups_total;
+	boolean verif=true;
 
 	
 	
@@ -23,15 +18,18 @@ public class Numbers{
 	}
 	
 	public void start(){
-		Scanner input=new Scanner (System.in);
+	while(verif){
+		try {
 	
+		Scanner input=new Scanner (System.in);
+		
 		for(this.coup_init=1;this.coups_total>=this.coup_init;this.coup_init++){
 			this.cp_essai--;
 		System.out.println("Essai n° "+this.coup_init+"\nIl vous reste "+(this.cp_essai)+" essai(s)");
-		System.out.println("Entrez le nombre");
+		System.out.println("Entrez le nombre:");
 		
 		this.nb_chosen=input.nextInt();
-		if (this.coups_total<=this.coup_init){
+		if (this.cp_essai==0){
 			System.out.println("Vous avez perdu \nLe nombre magique était "+this.nb_magic);
 		}else{
 			if (this.nb_chosen>this.nb_magic){
@@ -48,8 +46,20 @@ public class Numbers{
 		
 		}
 		input.close();
+	} catch (Exception e) {
+		System.out.println("\nSaisie incorrecte. Le jeu recommence...\n");
+		this.coup_init=1;
+		this.cp_essai=5;
+		continue;
+	}
+		break;
+	}
 	}
 
+		
+	
+	
+	
 	public static void main(String[] args) {
 		Numbers jeu=new Numbers();
 		jeu.rand_val();
