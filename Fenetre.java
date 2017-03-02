a9f57edb5759f96465e070d25b0bb51a55c0003e
Laurent-PANEK <laurent.panek@ynov.com>
2017-03-02 12:05:31 +0100

    update fenetre

--- Fenetre.java
+++ Fenetre.java
@@ -1,4 +1,5 @@
 package com.classe;
+
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.CardLayout;
@@ -17,137 +18,145 @@ import javax.swing.JTextField;
 public class Fenetre extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-  CardLayout cl = new CardLayout();
-  JPanel content = new JPanel();
-  //Liste des noms de nos conteneurs pour la pile de cartes
-  String[] listContent = {"CARD_1", "CARD_2", "CARD_3", "CARD_4", "CARD_5"};
-  
-public Fenetre(){
-    this.setTitle("CardLayout");
-    this.setSize(300, 300);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLocationRelativeTo(null);
-		
-    JPanel menu = new JPanel();
-    menu.setBackground(Color.black);
-    JPanel mode = new JPanel();
-    mode.setBackground(Color.red);		
-    JPanel nombre = new JPanel();
-    nombre.setBackground(Color.green);
-    JPanel mot = new JPanel();
-    mot.setBackground(Color.blue);
-    JPanel plateau = new JPanel();
-    plateau.setBackground(Color.darkGray);
-    JPanel boutonPane = new JPanel();
-    
-    
-    JLabel label1 = new JLabel("Devinez le nombre magique");
-    JTextField answer = new JTextField(20);
-    JTextField nb_coup = new JTextField(5);
+	CardLayout cl = new CardLayout();
+	JPanel content = new JPanel();
+	// Liste des noms de nos conteneurs pour la pile de cartes
+	JPanel mode = new JPanel();
+	String[] listContent = { "CARD_1", "CARD_2", "CARD_3", "CARD_4", "CARD_5" };
+	private JButton goNB = new JButton("Nombre");
+	private JButton goMot = new JButton("Mot");
+	private JRadioButton infinite_chance = new JRadioButton("Coup Infini");
+	private JRadioButton fixed_chance = new JRadioButton("Coup maximum");
+	private JButton play = new JButton("Jouer");
+	private JTextField nb_coup = new JTextField(5);
+	private JPanel plateau = new JPanel();
+	private JButton ans = new JButton("Soumettre");
+	private String isMode;
+	private JLabel label1 = new JLabel("Devinez le nombre magique");
+	private JLabel label2 = new JLabel("Devinez le mot magique");
 
-    JButton goNB = new JButton("Nombre");
-    //Définition de l'action du bouton
-    goNB.addActionListener(new ActionListener(){
-      public void actionPerformed(ActionEvent event){
-        cl.show(content, listContent[1]);
-        /* Nombre jeu = new Nombre();
-        jeu.start(); */
-      }
-    });
-		
-    JButton goMOT = new JButton("Mot");
-    //Définition de l'action du bouton2
-    goMOT.addActionListener(new ActionListener(){
-      public void actionPerformed(ActionEvent event){				
-        cl.show(content, listContent[1]);
-        /* Mot jeu = new Mot(); */
-      }
-    });
-    
-    JButton ans = new JButton("Soumettre");
-    
-    ans.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			/* jeu.reponse(answer.getText(); */
-			
-		}
-	});
-    
-    JButton toMenu = new JButton("Menu");
-    
-    toMenu.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent event) {
-			cl.show(content, listContent[0]);
-		}
-	});
-    
-    JButton play = new JButton("Jouer");
-    
-    play.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			cl.show(content, listContent[4]);
-			/*jeu.setNBcoup();
-			 * jeu.start();*/
-		}
-	});
-    /*infinite = jeu.coupIllimite();
-     * fixed = jeu.coupDef();*/
-    JRadioButton infinite_chance = new JRadioButton("Coup Infini");
-    //infinite_chance.setActionCommand(infinite);
-    infinite_chance.setSelected(true);
-    
-    JRadioButton fixed_chance = new JRadioButton("Coup maximum");
-    fixed_chance.setActionCommand(getWarningString());
-    
-    ButtonGroup group = new ButtonGroup();
-    group.add(infinite_chance);
-    group.add(fixed_chance);
-    
-    infinite_chance.addActionListener(this);
-    fixed_chance.addActionListener(this);
-    
-    if (fixed_chance.getSelectedObjects() == null ) {
-    	nb_coup.setVisible(false);
+	public Fenetre() {
+
+		this.setTitle("CardLayout");
+		this.setSize(600, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+
+		JPanel boutonPane = new JPanel();
+
+		JButton toMenu = new JButton("Menu");
+
+		toMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				cl.show(content, listContent[0]);
+			}
+		});
+
+		JPanel menu = this.defMenu();
+		this.defMode();
+		this.defPlateau();
+
+		boutonPane.add(toMenu);
+		// On définit le layout
+		content.setLayout(cl);
+		// On ajoute les cartes à la pile avec un nom pour les retrouver
+		content.add(menu, listContent[0]);
+		content.add(this.mode, listContent[1]);
+		content.add(this.plateau, listContent[4]);
+
+		this.getContentPane().add(boutonPane, BorderLayout.SOUTH);
+		this.getContentPane().add(content, BorderLayout.CENTER);
+		this.setVisible(true);
 	}
-    else {
-    	nb_coup.setVisible(true);
-    }
-    
-    mode.add(infinite_chance);
-    mode.add(fixed_chance);
-    mode.add(nb_coup);
-    answer.addActionListener(this);
-    plateau.add(label1);
-    plateau.add(answer);
-    plateau.add(ans);
-    menu.add(goNB);
-    menu.add(goMOT);
-    mode.add(play);
-    boutonPane.add(toMenu);
-    //On définit le layout
-    content.setLayout(cl);
-    //On ajoute les cartes à la pile avec un nom pour les retrouver
-    content.add(menu, listContent[0]);
-    content.add(mode, listContent[1]);
-    content.add(nombre, listContent[2]);
-    content.add(mot, listContent[3]);
-    content.add(plateau, listContent[4]);
 
-    this.getContentPane().add(boutonPane, BorderLayout.SOUTH);
-    this.getContentPane().add(content, BorderLayout.CENTER);
-    this.setVisible(true);
-  }
-  
-  public static void main(String[] args) {
-		
+	public JPanel defMenu() {
+		JPanel menu = new JPanel();
+		menu.setBackground(Color.gray);
+
+		JLabel titre = new JLabel("LE NOM DU JEU");
+
+		// Définition de l'action du bouton
+		this.goNB.addActionListener(this);
+
+		// Définition de l'action du bouton2
+		this.goMot.addActionListener(this);
+
+		menu.add(titre);
+		menu.add(this.goNB);
+		menu.add(this.goMot);
+
+		return menu;
+
+	}
+
+	public void defMode() {
+		this.mode.setBackground(Color.red);
+
+		this.play.addActionListener(this);
+
+		ButtonGroup group = new ButtonGroup();
+
+		group.add(this.infinite_chance);
+		group.add(this.fixed_chance);
+
+		this.infinite_chance.setSelected(true);
+		this.infinite_chance.addActionListener(this);
+		this.fixed_chance.addActionListener(this);
+
+		this.nb_coup.setVisible(false);
+
+		this.mode.add(this.infinite_chance);
+		this.mode.add(this.fixed_chance);
+		this.mode.add(this.nb_coup);
+		this.mode.add(this.play);
+
+	}
+
+	public void defPlateau() {
+		plateau.setBackground(Color.lightGray);
+		JTextField answer = new JTextField(20);
+
+		ans.addActionListener(this);
+		answer.addActionListener(this);
+
+		plateau.add(answer);
+		plateau.add(ans);
+
+	}
+
+	public static void main(String[] args) {
+
 		Fenetre fen = new Fenetre();
 
 	}
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if (source == goMot) {
+			cl.show(content, listContent[1]);
+			isMode = "mot";
+			plateau.remove(label1);
+			plateau.add(label2);
+			plateau.validate();
+		} else if (source == goNB) {
+			cl.show(content, listContent[1]);
+			isMode = "nb";
+			plateau.remove(label2);
+			plateau.add(label1);
+			plateau.validate();
+		} else if (source == play) {
+			cl.show(content, listContent[4]);
+		} else if (source == fixed_chance) {
+			if (this.fixed_chance.isSelected()) {
+				this.nb_coup.setVisible(true);
+				this.mode.validate();
+			}
+		} else if (source == infinite_chance) {
+			if (this.infinite_chance.isSelected()) {
+				this.nb_coup.setVisible(false);
+				this.mode.validate();
+			}
+		}
+	}
 }
-}
-
