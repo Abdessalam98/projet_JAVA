d1600a60d746e78cfdc1c102867c72e2cf0f743a
Laurent-PANEK <laurent.panek@ynov.com>
2017-03-27 11:23:45 +0200

    update

--- Mode.java
+++ Mode.java
@@ -1,12 +1,25 @@
 package com.classe;
 
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 class Mode {
 	protected int coup_total = 0;
 	protected int coup_init = 1;
 	protected int nbCoup;
 	protected int cp_essai;
 	protected boolean coupIllimite = false;
+	protected JTextField answer;
+	protected JLabel labelReponse;
+	protected JLabel labelInfo;
 
+	public Mode(JTextField answer, JLabel labelReponse, JLabel labelInfo) {
+		// TODO Auto-generated constructor stub
+		this.answer = answer;
+		this.labelInfo = labelInfo;
+		this.labelReponse = labelReponse;
+	}
+	
 	protected void setCoup(int nbCoup) {
 		this.coup_total = nbCoup;
 		this.cp_essai = nbCoup;
@@ -15,5 +28,13 @@ class Mode {
 	
 	protected void setIllimite() {
 		this.coupIllimite = true;
+	}
+	
+	protected void getInfo() {
+		if (coupIllimite) {
+			labelInfo.setText("Essai n° " + this.coup_init + "\n Il vous reste × essai(s)");				
+		} else {
+			labelInfo.setText("Essai n° " + this.coup_init + "\n Il vous reste " + (this.cp_essai) + " essai(s)");
+		}
 	}
 }
